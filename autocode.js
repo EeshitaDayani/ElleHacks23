320a735ef69a6d35a1af476883af76cfbbe70d5e
Abirami Karthikeyan <97060120+k-abirami@users.noreply.github.com>
2023-02-19 06:02:05 -0500

    Add files via upload
    Added main func and API call to autocode

---
+++ autocode.js
@@ -0,0 +1,8 @@
+// Using Node.js 14.x +
+// use "lib" package from npm
+const lib = require('lib')({token: "tok_dev_PgAKpZ1he4MvMpnL6d7w71zXRByCX1eps7BKXaAkG5RuRMaTVnZTRKwF56Vg9o3y"});
+
+// make API request
+let result =  lib.edayani.ellehacks['@0.0.2']({
+  number: `+14168482770`
+});
